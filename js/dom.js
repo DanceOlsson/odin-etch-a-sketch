d8b67677eccdc7af07079294e54ad7f396f3a808
Alexander Olsson <danceolsson@gmail.com>
2024-11-27 12:32:02 +0100

    start creating a grid and add css file with styling

---
+++ js/dom.js
@@ -0,0 +1,7 @@
+const container = document.getElementById("container");
+
+// Create a grid
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.classList.add('grid-row');
+}
