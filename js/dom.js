a6120ac3e6e9ba370609f02cd62246e4b3661d5a
Alexander Olsson <danceolsson@gmail.com>
2024-11-27 16:09:35 +0100

    complete grid function and add css link in index.html

--- js/dom.js
+++ js/dom.js
@@ -1,7 +1,19 @@
 const container = document.getElementById("container");
 
-// Create a grid
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add('grid-row');
+// Function that creates a grid
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add('grid-row');
+
+        // nested loop creating horisontal boxes inside each row
+        for (let j = 0; j < size; j++) {
+            const box = document.createElement('div');
+            box.classList.add('grid-box');
+            row.appendChild(box);
+        }
+        container.appendChild(row);
+    }
 }
+
+createGrid(16);
